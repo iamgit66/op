db80c100fa68266c713216dbcb204967af57676d
iamgit66 <71644333+iamgit66@users.noreply.github.com>
2020-11-15 12:06:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package/lean/
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+git clone https://github.com/iamgit66/luci-app-poweroff.git
